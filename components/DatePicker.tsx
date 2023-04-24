06cfbd65e404faa48e5668a72f5e2334e9986eb4
sethbarton <seth.barton@integral.io>
2023-04-24 11:05:17 -0600

    Refactor out DayButton

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -5,16 +5,8 @@ export function DatePicker() {
     <div>
       <label>January</label>
       {daysOfTheMonth.map((day) => {
-        return day === DateTime.now().day ? (
-          <>
-            <input type={"radio"} id={`${day}-button`} checked={true} />
-            <label htmlFor={`${day}-button`}>{day}</label>
-          </>
-        ) : (
-          <>
-            <input type={"radio"} id={`${day}-button`} />
-            <label htmlFor={`${day}-button`}>{day}</label>
-          </>
+        return (
+          <DayButton key={day} day={day} checked={day === DateTime.now().day} />
         );
       })}
     </div>
@@ -25,3 +17,16 @@ const daysOfTheMonth = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30, 31,
 ];
+
+function DayButton(props: { day: number; checked: boolean }) {
+  return (
+    <>
+      <input
+        type={"radio"}
+        id={`${props.day}-button`}
+        checked={props.checked}
+      />
+      <label htmlFor={`${props.day}-button`}>{props.day}</label>
+    </>
+  );
+}
